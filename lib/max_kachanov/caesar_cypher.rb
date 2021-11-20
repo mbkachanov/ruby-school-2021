be2d629bc99aef64f15667b385b5fc6ec8f45ada
Maxim Kachanov <m.b.kachanov@gmail.com>
2021-11-20 14:37:13 +0200

    implements CaesarCypher

--- lib/max_kachanov/caesar_cypher.rb
+++ lib/max_kachanov/caesar_cypher.rb
@@ -3,12 +3,15 @@ module MaxKachanov
     attr_reader :input_string, :shift
 
     def initialize(input_string: '', shift: 0)
-      @input_string = input_string
-      @shift = shift
+      @input_string = input_string.to_s
+      @shift = shift.to_i
     end
 
     def encrypt
-      input_string
+      alphabet = [*'a'..'z', *'A'..'Z'].join
+      shifted_alphabet = [[*'a'..'z'].rotate(shift), [*'A'..'Z'].rotate(shift)].join
+
+      input_string.tr(alphabet, shifted_alphabet)
     end
   end
 end
--- spec/max_kachanov/caesar_cypher_spec.rb
+++ spec/max_kachanov/caesar_cypher_spec.rb
@@ -4,12 +4,21 @@ RSpec.describe MaxKachanov::CaesarCypher do
   subject { described_class.new(input_string: input_string, shift: shift) }
 
   describe '#encrypt' do
+    let(:input_string) { 'aAbBrRzZ 2021!' }
+
     context 'if shift is not specified' do
-      let(:input_string) { 'Hello!' }
       let(:shift) { nil }
 
       it 'returns initial input_string' do
         expect(subject.encrypt).to eq(input_string)
+      end
+    end
+
+    context 'if shift is specified' do
+      let(:shift) { 3 }
+
+      it 'returns encrypts input_string properly' do
+        expect(subject.encrypt).to eq('dDeEuUcC 2021!')
       end
     end
   end
